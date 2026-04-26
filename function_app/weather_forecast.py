0c6475b6ec6b1fb9e9a7c385f65feec8711a01ee
jaehwachung <jaehwachung@knou.ac.kr>
2026-04-26 17:36:16 +0900

    Refactor weather forecast function to use FunctionApp

--- function_app/weather_forecast.py
+++ function_app/weather_forecast.py
@@ -5,7 +5,10 @@ from urllib.request import urlopen
 from typing import List
 
 
-def main(req: func.HttpRequest) -> func.HttpResponse:
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+
+@app.route(route="http_trigger")
+def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     weather_object = None
