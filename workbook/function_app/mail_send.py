88feadf82d35531d1477639d9f23086b1daf6053
Jiho Persy Lee <search5@gmail.com>
2026-06-09 21:33:45 +0900

    Update mail_send.py

--- workbook/function_app/mail_send.py
+++ workbook/function_app/mail_send.py
@@ -12,6 +12,7 @@ def mallSendMail(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     mail_title = req.params.get('mail_title')
+    mail_receiver = req.params.get('receiver')
     if not mail_title:
         try:
             req_body = req.get_json()
@@ -19,13 +20,14 @@ def mallSendMail(req: func.HttpRequest) -> func.HttpResponse:
             pass
         else:
             mail_title = req_body.get('mail_title')
+            mail_receiver = req_body.get('receiver')
 
     if mail_title:
         smtp = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-        smtp.login('<me>@knou.ac.kr', '<발급비밀번호>')
+        smtp.login('<me>@knou.ac.kr', '<앱비밀번호>')
         msg = MIMEText(mail_title)
         msg['Subject'] = Header('클라우드 컴퓨팅 메일 보내기 예제입니다', 'utf-8')
-        smtp.sendmail('<me>@knou.ac.kr', '<to>', msg.as_string())
+        smtp.sendmail('<me>@knou.ac.kr', mail_receiver, msg.as_string())
         smtp.quit()
 
         return func.HttpResponse(f"Hello {mail_title}!")
